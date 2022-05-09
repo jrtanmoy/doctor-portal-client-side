442e376055ef4b26b0d374e3ecd602ddbf12fd5c
jrtanmoy <roy28301@gmail.com>
2022-05-09 22:49:54 +0600

    created services section

---
+++ src/Pages/Home/Services.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import fluoride from '../../assets/images/fluoride.png';
+import cavity from '../../assets/images/cavity.png';
+import whitening from '../../assets/images/whitening.png';
+import Service from './Service';
+import treatment from '../../assets/images/treatment.png'
+
+const Services = () => {
+    const services = [
+        {
+            _id: 1,
+            name: 'Fluoride Treatment',
+            description: '',
+            img: fluoride
+        },
+        {
+            _id: 2,
+            name: 'Cavity Filling',
+            description: '',
+            img: cavity
+        },
+        {
+            _id: 3,
+            name: 'Teeth Whitening',
+            description: '',
+            img: whitening
+        },
+    ];
+    return (
+        <div className='my-28'>
+            <div className='text-center'>
+                <h3 className='text-primary  text-xl font-bold uppercase'>Our Services</h3>
+                <h2 className='text-4xl'>Services We Provide</h2>
+            </div>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+                {
+                    services.map(service => <Service
+                        key={service._id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
+            <div class="hero min-h-screen px-16">
+                <div class="hero-content flex-col lg:flex-row">
+                    <img src={treatment} class="max-w-sm rounded-lg shadow-2xl" alt='' />
+                    <div className='pl-4'>
+                        <h1 class="text-5xl font-bold">Exceptional Dental Care, On Yours Terms.</h1>
+                        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button class="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
