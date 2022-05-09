222eb50e79058816e5873bf69ff4b99222d27fb7
jrtanmoy <roy28301@gmail.com>
2022-05-09 21:44:01 +0600

    created top banner & update navber

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
 import Navber from './Pages/Shared/Navber';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </div>
   );
