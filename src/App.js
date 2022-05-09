259c19123853972fe7e75c9e3054a292c2e37419
jrtanmoy <roy28301@gmail.com>
2022-05-09 21:18:15 +0600

    add navber & set route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Navber from './Pages/Shared/Navber';
 
 function App() {
   return (
     <div>
-      <button class="btn btn-primary">Button</button>
+      <Navber></Navber>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
