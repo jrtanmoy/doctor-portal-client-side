e2b5281c70bd89acb95206cc96b15f9500c4047d
jrtanmoy <roy28301@gmail.com>
2022-05-11 12:30:13 +0600

    lift up state and available appointments

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import chair from '../../assets/images/chair.png';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
-import { format } from 'date-fns';
 
-const AppointmentBanner = () => {
-    const [date, setDate] = useState(new Date());
+const AppointmentBanner = ({date, setDate}) => {
 
     return (
         <div class="hero min-h-screen">
@@ -17,12 +15,10 @@ const AppointmentBanner = () => {
                         selected={date}
                         onSelect={setDate}
                     />
-                    <p>You have selected: {format(date, 'PP')}</p>
                 </div>
             </div>
 
         </div>
     );
 };
-
 export default AppointmentBanner;
